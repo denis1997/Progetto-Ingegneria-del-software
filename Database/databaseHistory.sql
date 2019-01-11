7d9831593a50da63a23008ec3dd712319ca3c012
Jessicaleone <45167493+Jessicaleone@users.noreply.github.com>
2019-01-11 16:34:11 +0100

    Update databaseHistory.sql

--- Database/databaseHistory.sql
+++ Database/databaseHistory.sql
@@ -14,4 +14,23 @@ create table storia(
     IDsensore integer unsigned default null,
     constraint storia_esnsore foreign key(IDsensore) references sensore(ID) on update cascade
 
-)
+);
+
+insert into `sensore` values (01,'t1');
+insert into `sensore` values (02,'t2');
+insert into `sensore` values (03,'u1');
+insert into `sensore` values (04,'u2');
+insert into `sensore` values (05,'l1');
+insert into `sensore` values(06,'l2');
+insert into `sensore` values (07,'p1');
+insert into `sensore` values (08,'p2');
+
+insert into `storia` values (1, '24°',t1);
+insert into `storia` values (1, '10°',t2);
+insert into `storia` values (1, '6°',t3);
+insert into `storia` values (1, '54%',u1);
+insert into `storia` values (1, '50pa',p1);
+insert into `storia` values (1, '5 lux',l1);
+
+
+
