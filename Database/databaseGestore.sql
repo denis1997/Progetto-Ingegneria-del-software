2cc99da84c813d13c164e14769360afe29cce687
Coccio98 <31818421+Coccio98@users.noreply.github.com>
2019-01-12 11:57:19 +0100

    modifiche varie

--- Database/databaseGestore.sql
+++ Database/databaseGestore.sql
@@ -11,15 +11,15 @@ create table gestore(
     IDArea integer unsigned not null
 );
 
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0001, 'gestore1', 'op0001', 'edificio', 01);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0002, 'gestore2', 'op0002', 'edificio', 01);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0003, 'gestore3', 'op0003', 'città', 02);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0004, 'gestore4', 'op0004', 'zona', 03);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0005, 'gestore5', 'op0005', 'zona', 03);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0006, 'gestore6', 'op0006', 'città', 02);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0007, 'gestore7', 'op0007', 'zona', 03);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0008, 'gestore8', 'op0008', 'città', 02);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0009, 'gestore9', 'op0009', 'edificio', 01);
-insert into gestore (ID, username, password_, tipoArea,IDarea) values (0010, 'gestore10', 'op0010', 'edificio', 01);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore1', 'op0001', 'edificio', 1);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore2', 'op0002', 'edificio', 2);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore3', 'op0003', 'città', 1);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore4', 'op0004', 'zona', 1);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore5', 'op0005', 'zona', 2);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore6', 'op0006', 'città', 2);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore7', 'op0007', 'zona', 3);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore8', 'op0008', 'città', 3);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore9', 'op0009', 'edificio', 3);
+insert into gestore (ID, username, password_, tipoArea,IDarea) values (default, 'gestore10', 'op0010', 'edificio', 4);
 
 select * from gestore
