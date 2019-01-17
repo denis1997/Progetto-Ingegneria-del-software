3630d5a34d76e8fed82c236e2735f1cde06a57ce
Denis Di Patrzio <denisdipatrizio4@gmail.com>
2019-01-17 23:43:25 +0100

    Deliverable 3 e DashboardAmministratore

---
+++ Dashboard Amministratore/Dashboard Amministratore/Form1.vb
@@ -0,0 +1,33 @@
+﻿
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Form2.Show()
+        Form2.BringToFront()
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form3.Show()
+        Form3.BringToFront()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+End Class
---
+++ Dashboard Amministratore/Dashboard Amministratore/Form2.vb
@@ -0,0 +1,40 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form2
+    Dim connection As New MySqlConnection
+    Dim command As MySqlCommand
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        connection = New MySqlConnection
+        connection.ConnectionString = "server=localhost; username=root; password=denis1997; database=databasegestore;"
+        Dim READER As MySqlDataReader
+
+
+        Try
+            connection.Open()
+            Dim query As String
+            query = "insert into databasegestore.gestore (ID , username , password_ , tipoArea, IDArea) values('" & TextBox1.Text & "', '" & TextBox2.Text & "', '" & TextBox3.Text & "', '" & TextBox4.Text & "', '" & TextBox5.Text & "'  )"
+            command = New MySqlCommand(query, connection)
+            READER = command.ExecuteReader
+
+            MessageBox.Show("Nuovo gestore aggiunto")
+
+            connection.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Dispose()
+
+        End Try
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
