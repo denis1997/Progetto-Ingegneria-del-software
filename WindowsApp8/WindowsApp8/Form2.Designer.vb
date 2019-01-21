1d9a038897d116306a335449a51307a914b89ca5
lucadisab <ds97luca@gmail.com>
2019-01-21 01:15:06 +0100

    mim

--- WindowsApp8/WindowsApp8/Form2.Designer.vb
+++ WindowsApp8/WindowsApp8/Form2.Designer.vb
@@ -36,85 +36,93 @@ Partial Class Form2
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(306, 165)
+        Me.Label4.Location = New System.Drawing.Point(115, 69)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(716, 32)
+        Me.Label4.Size = New System.Drawing.Size(271, 13)
         Me.Label4.TabIndex = 15
         Me.Label4.Text = "L'id specifico di un sensore deve iniziare con 't','u','p' o 'l'"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(120, 509)
+        Me.Label3.Location = New System.Drawing.Point(45, 213)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(102, 32)
+        Me.Label3.Size = New System.Drawing.Size(40, 13)
         Me.Label3.TabIndex = 14
         Me.Label3.Text = "IDArea"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(443, 503)
+        Me.TextBox2.Location = New System.Drawing.Point(166, 211)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(446, 38)
+        Me.TextBox2.Size = New System.Drawing.Size(170, 20)
         Me.TextBox2.TabIndex = 13
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(120, 290)
+        Me.Label2.Location = New System.Drawing.Point(45, 122)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(159, 32)
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
         Me.Label2.TabIndex = 12
         Me.Label2.Text = "IDSpecifico"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(65, 721)
+        Me.Button1.Location = New System.Drawing.Point(24, 302)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(446, 114)
+        Me.Button1.Size = New System.Drawing.Size(167, 48)
         Me.Button1.TabIndex = 11
         Me.Button1.Text = "Aggiungi sensore in AreePiano "
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(443, 290)
+        Me.TextBox1.Location = New System.Drawing.Point(166, 122)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(446, 38)
+        Me.TextBox1.Size = New System.Drawing.Size(170, 20)
         Me.TextBox1.TabIndex = 10
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(1248, 24)
+        Me.Label1.Location = New System.Drawing.Point(468, 10)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(167, 32)
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
         Me.Label1.TabIndex = 9
         Me.Label1.Text = "Elenco aree"
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 31
-        Me.ListBox1.Location = New System.Drawing.Point(1229, 67)
+        Me.ListBox1.Location = New System.Drawing.Point(461, 28)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(276, 841)
+        Me.ListBox1.Size = New System.Drawing.Size(106, 355)
         Me.ListBox1.TabIndex = 8
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(672, 721)
+        Me.Button2.Location = New System.Drawing.Point(252, 302)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(446, 114)
+        Me.Button2.Size = New System.Drawing.Size(167, 48)
         Me.Button2.TabIndex = 16
         Me.Button2.Text = "Aggiungi sensore in aree Areeperto"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Form2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1517, 920)
+        Me.ClientSize = New System.Drawing.Size(577, 385)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -124,6 +132,7 @@ Partial Class Form2
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ListBox1)
+        Me.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Name = "Form2"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form2"
